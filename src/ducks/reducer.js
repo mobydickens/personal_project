342c02d77505152cca375c5afc00fcb8f34a2258
mobydickens <steadmegan@gmail.com>
2018-12-17 14:34:58 -0700

    Installed redux, react-redux, redux devTools; created store and reducer file, imported provider and store into index.js.

---
+++ src/ducks/reducer.js
@@ -0,0 +1,15 @@
+let initialState = {
+
+}
+
+// ACTION CONSTANTS
+
+// ACTION CREATORS
+
+// REDUCER
+export default function reducer(state=initialState, action) {
+  switch(action.type) {
+    default: 
+      return state;
+  }
+}
