542606ace2f93eb1dc58dcc1fc85d0f0901ab1fe
mobydickens <steadmegan@gmail.com>
2018-12-17 12:39:11 -0700

    Working more with tailwind on design.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,30 +4,35 @@ class Main extends Component {
   render() {
     return (
       <div>
-         <div className='xs:text-2xl xs:m-4 lg:text-3xl lg:m-6'>Name</div>
+        <div className='flex justify-between'>
+          <div className='text-2xl m-4 lg:text-3xl lg:m-6'>Name</div>
+          <div className='visible sm:invisible lg:visible m-6'>Already a member? 
+            <span className='cursor-pointer hover:grey'> Login</span>
+          </div>
+        </div>
         <div>
-          <div className='xs:text-2xl xs:mx-4 xs:my-6 lg:m-8 lg:h-32'>Your place for organizing and staying on track.</div>
+          <div className='text-2xl mx-4 my-6 lg:m-8 lg:h-32'>Your place for organizing and staying on track.</div>
         </div>
         <div className='flex items-center flex-col'>
           <button 
             className='bg-green hover:bg-green-dark text-white border border-green-base py-2 px-4 m-4 rounded'>
             Sign Up
           </button> 
-          <p className='mb-6'>Already a member? 
+          <p className='lg:invisible mb-6'>Already a member? 
             <span className='cursor-pointer hover:grey'> Login</span>
           </p>
         </div>
-        <div className='bg-green w-screen flex justify-center'>
+        <div className='bg-green w-screen flex flex-col items-center lg:flex-row lg:justify-center'>
           <img
-            className='xs:w-3/5 m-6 lg:w-auto lg:h-48'
+            className='w-3/5 m-6 lg:w-auto lg:h-48'
             src="https://vignette.wikia.nocookie.net/the-darkest-minds/images/4/47/Placeholder.png/revision/latest?cb=20160927044640" 
             alt="placeholder" />
           <img
-            className='xs:w-3/5 m-6 lg:w-auto lg:h-48'
+            className='w-3/5 m-6 lg:w-auto lg:h-48'
             src="https://vignette.wikia.nocookie.net/the-darkest-minds/images/4/47/Placeholder.png/revision/latest?cb=20160927044640" 
             alt="placeholder" />
             <img
-            className='xs:w-3/5 m-6 lg:w-auto lg:h-48'
+            className='w-3/5 m-6 lg:w-auto lg:h-48'
             src="https://vignette.wikia.nocookie.net/the-darkest-minds/images/4/47/Placeholder.png/revision/latest?cb=20160927044640" 
             alt="placeholder" />
         </div>
--- tailwind.js
+++ tailwind.js
@@ -167,7 +167,6 @@ module.exports = {
   */
 
   screens: {
-    'xs': '360px',
     'sm': '576px',
     'md': '768px',
     'lg': '992px',
