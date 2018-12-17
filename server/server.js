9558e33bffe5c6ed513337238e7340651bb4c9a9
mobydickens <steadmegan@gmail.com>
2018-12-17 12:20:17 -0700

    Main page initial styling.

--- server/server.js
+++ server/server.js
@@ -3,9 +3,18 @@ const express = require('express');
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 const massive = require('massive');
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
 
 const app = express();
 app.use(express.json());
 
-app.listen(SERVER_PORT, () => console.log(`server listening at port ${SERVER_PORT}`));
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db);
+  app.listen(SERVER_PORT, () => console.log(`server listening at port ${SERVER_PORT}`));
+});
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,16 +4,32 @@ class Main extends Component {
   render() {
     return (
       <div>
-         <h1>Name</h1>
+         <div className='xs:text-2xl xs:m-4 lg:text-3xl lg:m-6'>Name</div>
         <div>
-          <h3>Brief Site Description</h3>
-          <p>Placeholder description</p>
+          <div className='xs:text-2xl xs:mx-4 xs:my-6 lg:m-8 lg:h-32'>Your place for organizing and staying on track.</div>
         </div>
-        <button>Sign Up</button>
-        <p>Already a member? <span>Sign In.</span></p>
-        <div>
-          {/* <img src="" alt="placeholder"/> */}
-          {/* <img src="" alt="placeholder"/> */}
+        <div className='flex items-center flex-col'>
+          <button 
+            className='bg-green hover:bg-green-dark text-white border border-green-base py-2 px-4 m-4 rounded'>
+            Sign Up
+          </button> 
+          <p className='mb-6'>Already a member? 
+            <span className='cursor-pointer hover:grey'> Login</span>
+          </p>
+        </div>
+        <div className='bg-green w-screen flex justify-center'>
+          <img
+            className='xs:w-3/5 m-6 lg:w-auto lg:h-48'
+            src="https://vignette.wikia.nocookie.net/the-darkest-minds/images/4/47/Placeholder.png/revision/latest?cb=20160927044640" 
+            alt="placeholder" />
+          <img
+            className='xs:w-3/5 m-6 lg:w-auto lg:h-48'
+            src="https://vignette.wikia.nocookie.net/the-darkest-minds/images/4/47/Placeholder.png/revision/latest?cb=20160927044640" 
+            alt="placeholder" />
+            <img
+            className='xs:w-3/5 m-6 lg:w-auto lg:h-48'
+            src="https://vignette.wikia.nocookie.net/the-darkest-minds/images/4/47/Placeholder.png/revision/latest?cb=20160927044640" 
+            alt="placeholder" />
         </div>
       </div>
     );
