6ba5c23fba70d6b0074a9b6d557ca28ab8a7fa24
mobydickens <steadmegan@gmail.com>
2018-12-22 14:15:28 -0700

    The percentage calculations seem to be working. I need to figure out what to do when it's a day we haven't hit yet...

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -41,14 +41,17 @@ class Table extends Component {
 
   //table helper function
   createTableRows = () => {
+    //setting needed variables
     const { taskInfo, timelogs, daily_dev_hours, start_date } = this.state;
     let rows = [];
     let initialEstimate = 0;
+    let devHours_expected = 0;
+    let devHours_actual = 0
     let date = moment(start_date); 
     let today = new Date();
     let logDate = 0;
     today.setHours(0,0,0,0);
-    console.log(today)
+
     //getting initial estimate to find out how many rows I need in my table
     for (let i = 0; i < taskInfo.length; i++) {
       initialEstimate += Number(taskInfo[i].initial_estimate);
@@ -57,8 +60,10 @@ class Table extends Component {
     let rowsNeeded = Math.ceil(initialEstimate / daily_dev_hours);
     //configuring how many hours were spent EACH DAY
     for (let i = 1; i <= rowsNeeded; i++) {
+      devHours_expected += daily_dev_hours;
       let actual_hours_today = 0;
       let totalEstimateChange = 0;
+      
       for (let j = 0; j < timelogs.length; j++) {
         let logged_date = moment(timelogs[j].created_at).format('L'); 
         logDate = new Date(logged_date);
@@ -67,21 +72,39 @@ class Table extends Component {
         }
         //get estimate change for project based on changes made to estimates in each timelog
         totalEstimateChange += Number(timelogs[j].estimate_change);
+
       }
       //get dates for column dates, skipping weekends
       let dateCheck = date.format('dddd');
       if(dateCheck.includes("Saturday")) {
         date.add(2, 'days');
       }
-      //calculate current estimate per day for the rows
+      //calculate current estimate per day for the rows (only need to recalculate if the date is NOW or later)
       let currentEstimate = initialEstimate;
       if( logDate >= today ) {
         currentEstimate = initialEstimate - totalEstimateChange;
       }
 
-      rows.push( { date: date.format('L dddd'), expected_hours: daily_dev_hours, actual_hours_today: actual_hours_today, currentEstimate: currentEstimate } )
+      //calculate expected % of project complete based on dev hours per day that we alloted to work
+      // total hours (initial estimate - dev hours per day (added up to the date) )
+      let percent_expected = Number((( devHours_expected / initialEstimate ) * 100).toFixed(2));
+      devHours_actual += actual_hours_today;
+      let percent_actual = Number((( devHours_actual / initialEstimate) * 100).toFixed(2));
+      console.log('% actual: ', percent_actual);
+
+      rows.push( { 
+        date: date.format('L dddd'), 
+        expected_hours: daily_dev_hours, 
+        actual_hours_today: actual_hours_today, 
+        currentEstimate: currentEstimate,
+        percent_expected: percent_expected,
+        percent_actual: percent_actual,
+        hours_expected: '',
+        hours_remaining: '' } )
+
       date.add(1, 'day');
     }
+    // console.log()
     console.log("Initial Estimate: ", initialEstimate, "Rows Needed: ", rowsNeeded, "Current Rows: ", rows)
   }
 
