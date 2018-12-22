1f408edfd0fa53ca85e8a34112d2a9693dec8963
mobydickens <steadmegan@gmail.com>
2018-12-22 13:40:24 -0700

    Tried to calculate initial estimate. Still need further testing to make sure it works well.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -45,27 +45,43 @@ class Table extends Component {
     let rows = [];
     let initialEstimate = 0;
     let date = moment(start_date); 
+    let today = new Date();
+    let logDate = 0;
+    today.setHours(0,0,0,0);
+    console.log(today)
     //getting initial estimate to find out how many rows I need in my table
     for (let i = 0; i < taskInfo.length; i++) {
       initialEstimate += Number(taskInfo[i].initial_estimate);
     }
     //get how many rows I need in table based on initial estimate
     let rowsNeeded = Math.ceil(initialEstimate / daily_dev_hours);
-    //get dates for column dates, skipping weekends
+    //configuring how many hours were spent EACH DAY
     for (let i = 1; i <= rowsNeeded; i++) {
+      let actual_hours_today = 0;
+      let totalEstimateChange = 0;
+      for (let j = 0; j < timelogs.length; j++) {
+        let logged_date = moment(timelogs[j].created_at).format('L'); 
+        logDate = new Date(logged_date);
+        if( logged_date === date.format('L') ) {
+          actual_hours_today += Number(timelogs[j].spent_time);
+        }
+        //get estimate change for project based on changes made to estimates in each timelog
+        totalEstimateChange += Number(timelogs[j].estimate_change);
+      }
+      //get dates for column dates, skipping weekends
       let dateCheck = date.format('dddd');
       if(dateCheck.includes("Saturday")) {
-        console.log("hit?")
         date.add(2, 'days');
       }
-        // let spentTime
-      rows.push( { date: date.format('L dddd'), expected_hours: daily_dev_hours } )
-      date.add(1, 'day');
-      console.log(date.format('L dddd'))
-    }
-  
+      //calculate current estimate per day for the rows
+      let currentEstimate = initialEstimate;
+      if( logDate >= today ) {
+        currentEstimate = initialEstimate - totalEstimateChange;
+      }
 
- 
+      rows.push( { date: date.format('L dddd'), expected_hours: daily_dev_hours, actual_hours_today: actual_hours_today, currentEstimate: currentEstimate } )
+      date.add(1, 'day');
+    }
     console.log("Initial Estimate: ", initialEstimate, "Rows Needed: ", rowsNeeded, "Current Rows: ", rows)
   }
 
