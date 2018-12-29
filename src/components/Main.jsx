91c85043afc0a892dc89f9b830c781f5083fdb8f
mobydickens <steadmegan@gmail.com>
2018-12-29 12:50:55 -0700

    More styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,19 +13,17 @@ class Main extends Component {
         <div>
           <MainHeader />
         </div>
-        <div className='flex m-2 p-2 flex-col lg:m-12 lg:pt-10 lg:flex-row lg:justify-center'>
+        <div className='bg-image-main h-screen'></div>
+        <div className='flex m-2 p-2 flex-col lg:m-6 lg:flex-row lg:justify-center'>
           <div className='flex flex-col w-full lg:w-1/2'>
-            <div className='font-josefin p-6 text-3xl lg:text-4xl lg:p-12'>Your place for organizing and staying on track.</div>
-            <div>
+            <div className='font-josefin p-6 text-3xl lg:text-4xl z-10'>Your place for organizing and staying on track.</div>
+            <div className='z-10'>
               <button
                 onClick={ () => this.signup() } 
-                className='bg-green hover:bg-green-dark hover:border-green-dark text-white border border-green mx-6 lg:mx-12 py-2 lg:py-4 px-4 lg:px-8 text-xl rounded'>
+                className='bg-palette-grey hover:bg-palette-dark hover:border-palette-dark text-white border border-palette-grey mx-6 lg:mx-12 py-2 lg:py-4 px-4 lg:px-8 text-xl rounded z-10'>
                 Sign Up
               </button> 
             </div>
-          </div>
-          <div className='p-4 w-full lg:w-2/5'>
-            <img src="/images/wordcloud2.png" alt="wordcloud"/>
           </div>
         </div>
       </div>
--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -37,44 +37,44 @@ class MainHeader extends Component {
   }
 
   render() {
-    let login = <div className='flex flex-col lg:flex-row lg:border-b border-green py-2'>
+    let login = <div className='flex flex-col lg:flex-row'>
                   <input
                     value={ this.state.email }
                     placeholder='Email'
-                    className='input focus:outline-none mb-2'
+                    className='login-input border-b lg:border-white focus:outline-none mr-4'
                     onChange={ (e) => this.setState({ email: e.target.value }) } 
                     type="text"/> 
                   <input
                     value={ this.state.password }
                     placeholder='Password'
-                    className='input focus:outline-none mb-2 lg:mb-0'
+                    className='login-input border-b lg:border-white focus:outline-none mr-4'
                     onChange={ (e) => this.setState({ password: e.target.value }) } 
                     type="password"/>
                   <div className='flex'>
-                    <div>
-                      <button
-                        className='flex-no-shrink bg-green hover:bg-green-dark border-green hover:border-green-dark text-sm border-4 text-white py-1 px-2 rounded' 
-                        onClick={ () => this.login() }>LOGIN</button>
-                    </div>
-                    <div>
-                      <button 
-                        className='btn-white mt-2'
-                        onClick={ () => this.setState({ inputShowing: !this.state.inputShowing }) }>CANCEL
-                      </button>
-                    </div>
+                  <div>
+                    <button
+                      className='btn-reg mt-4 hover:bg-palette-dark hover:border-palette-dark mt-0 ml-2' 
+                      onClick={ () => this.login() }>LOGIN</button>
+                  </div>
+                  <div>
+                    <button 
+                      className='btn-white mt-8'
+                      onClick={ () => this.setState({ inputShowing: !this.state.inputShowing }) }>CANCEL
+                    </button>
+                  </div>
                   </div>
                 </div>
     return (
-      <div>
-        <div className='flex justify-between m-6'>
-          <div className='jello lg:text-3xl'><span className='text-green'>J</span>ello</div>
+      <div className='bg-palette-grey p-2'>
+        <div className='flex justify-between m-2'>
+          <div className='jello lg:text-2xl text-white'><span className='text-palette-blue'>J</span>ello</div>
             { this.state.inputShowing ? 
               <div className='hidden lg:block w-full max-w-sm'>
                 {login}
               </div> :
-              <div className='flex'>
-                <div className='invisible lg:visible text-sm font-josefin'>ALREADY A MEMBER? </div>
-                <div onClick={ () => this.showLogin() } className='btn-white'>LOGIN</div>
+              <div className='flex mt-2'>
+                <div className='invisible lg:visible text-sm text-white font-josefin'>ALREADY A MEMBER? </div>
+                <div onClick={ () => this.showLogin() } className='btn-white text-palette-blue'>LOGIN</div>
               </div>
           }
         </div>
