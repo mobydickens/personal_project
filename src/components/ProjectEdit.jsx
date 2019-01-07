29fc23dfd95f0f0b52a7b5a226a18b9fa071da03
mobydickens <steadmegan@gmail.com>
2019-01-07 09:35:10 -0700

    Added error message to ProjectEdit modal (start a new project) so that user cannot submit empty inputs or non-valid selects

--- src/components/ProjectEdit.jsx
+++ src/components/ProjectEdit.jsx
@@ -13,7 +13,8 @@ class ProjectEdit extends Component {
       description: '',
       team: '',
       start_date: '',
-      edit: false
+      edit: false,
+      fieldsRequired: false
     }
   }
 
@@ -21,19 +22,25 @@ class ProjectEdit extends Component {
     const { teams } = this.props;
     const { title, devHours, description, start_date } = this.state;
     let team_id = teams.filter(singleteam => singleteam.name === this.state.team);
-    try {
-      let res = await axios.post('/api/newproject', { title, devHours, description, team_id: team_id[0].id, start_date});
-      console.log("res.data project: ", res.data.project)
+    //if statement checks if any inputs are empty or unselected
+    if(!title || !devHours || !description || !start_date || !team_id[0]) {
       this.setState({
-        title: '',
-        devHours: '',
-        description: '',
-        team: '',
-        start_date: ''
+        fieldsRequired: true
       })
-      this.props.history.push(`/project/${res.data.project[0].id}`)
-    } catch (e) {
-      console.log('it errored!', e)
+    } else {
+      try {
+        let res = await axios.post('/api/newproject', { title, devHours, description, team_id: team_id[0].id, start_date});
+        this.setState({
+          title: '',
+          devHours: '',
+          description: '',
+          team: '',
+          start_date: ''
+        })
+        this.props.history.push(`/project/${res.data.project[0].id}`)
+      } catch (e) {
+        console.log('it errored!', e)
+      }
     }
   }
 
@@ -49,33 +56,36 @@ class ProjectEdit extends Component {
             <div className='flex justify-center'>
               <div className='mb-4 text-xl'>Start a new project</div>
             </div>
+            <div className='flex justify-center'>
+              {this.state.fieldsRequired ? <div className='text-red-lighter'>All fields required</div> : ""}
+            </div>
             <label htmlFor="title">Project Name:</label>
             <input
               autoFocus="autofocus"
-              onChange={ (e) => this.setState({ title: e.target.value })} 
+              onChange={ (e) => this.setState({ title: e.target.value, fieldsRequired: false })} 
               className='input-underlined focus:outline-none mb-4'  
               type="text" 
               value={ this.state.title}
             />
             <label htmlFor="desc">Description:</label><br/>
             <input 
-              onChange={ (e) => this.setState({ description: e.target.value }) }
+              onChange={ (e) => this.setState({ description: e.target.value, fieldsRequired: false }) }
               className='input-underlined focus:outline-none' 
             /><br/>
-            <select className='bg-grey-light border border-grey focus:outline-none' onChange={ (e) => this.setState( { team: e.target.value })}>
+            <select className='bg-grey-light border border-grey focus:outline-none' onChange={ (e) => this.setState( { team: e.target.value, fieldsRequired: false })}>
               <option defaultValue="selected">Choose team</option>
               {dropdown}
             </select><br/>
             <label htmlFor="devhours">Team dev hours available per day:</label>
             <input
-              onChange={ (e) => this.setState({ devHours: e.target.value }) }
+              onChange={ (e) => this.setState({ devHours: e.target.value, fieldsRequired: false }) }
               className='input-underlined focus:outline-none'  
               type="number"
               value={ this.state.devHours }
             />
             <label className='my-4' htmlFor="start">Start Date:</label>
             <input
-              onChange={ (e) => this.setState({ start_date: e.target.value })} 
+              onChange={ (e) => this.setState({ start_date: e.target.value, fieldsRequired: false })} 
               className='input-underlined focus:outline-none'  
               type="date"/>
             <div className='flex justify-center'>
@@ -87,7 +97,7 @@ class ProjectEdit extends Component {
               </div>
             </div>
             <div className='flex justify-center m-2'>
-              <button onClick={() => this.props.projectModalFn() }className='text-grey'>Cancel</button>
+              <button onClick={ () => this.props.projectModalFn() } className='text-grey'>Cancel</button>
             </div>
           </form>
         </div>
