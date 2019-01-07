824511006472e6e2c1d4aaf1e05bab13f12ad66a
mobydickens <steadmegan@gmail.com>
2019-01-07 10:36:55 -0700

    Login submit on Main Header Component works with key press of enter button

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -17,6 +17,14 @@ class MainHeader extends Component {
     }
   }
 
+  handlePassInput = (e) => {
+    this.setState({ 
+      password: e.target.value, 
+      incorrectPass: false, 
+      emailError: false 
+    })
+  }
+
   showLogin = () => {
     this.setState({
       inputShowing: !this.state.inputShowing
@@ -60,13 +68,19 @@ class MainHeader extends Component {
                     value={ this.state.password }
                     placeholder='Password'
                     className='shadow appearance-none rounded w-full py-2 px-3 text-grey-darker leading-tight bg-grey-lightest focus:outline-none focus:shadow-outline mt-2 lg:mt-0 lg:bg-palette-grey lg:border-b lg:border-grey-lightest lg:py-0 lg:text-grey-lighter'
-                    onChange={ (e) => this.setState({ password: e.target.value, incorrectPass: false, emailError: false }) } 
+                    onChange={ (e) => this.handlePassInput(e) }
+                    onKeyUp={event => {
+                      if (event.key === 'Enter') {
+                        this.login()
+                      }
+                    }}
                     type="password"/>
                   <div className='flex'>
                   <div>
                     <button
                       className='btn-reg mt-2 hover:bg-palette-dark hover:border-palette-dark ml-2 text-sm lg:mt-0' 
-                      onClick={ () => this.login() }>LOGIN</button>
+                      onClick={ () => this.login() }>LOGIN
+                    </button>
                   </div>
                   <div>
                     <button 
