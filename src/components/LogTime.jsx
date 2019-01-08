8a27f91e976dfc3bc54578fa27e7dc44b97b5ef4
mobydickens <steadmegan@gmail.com>
2019-01-08 11:49:34 -0700

    Inputs in the log time component now do not accept empty values. Error message will appear above if "add" button is pushed with null values.

--- src/components/LogTime.jsx
+++ src/components/LogTime.jsx
@@ -10,39 +10,49 @@ class LogTime extends Component {
       estimate_change: '',
       comment: '',
       taskId: this.props.taskId,
-      userId: this.props.userId
+      userId: this.props.userId,
+      fieldsRequired: false
     }
   }
 
   async newTimelog() {
-    await axios.post('/api/timelog', { ...this.state })
-    this.setState({
-      spent_time: '',
-      estimate_change: '',
-      comment: '',
-    })
-    this.props.getTasksAndLogs();
+    if(!this.state.spent_time || !this.state.estimate_change || !this.state.comment) {
+      this.setState({
+        fieldsRequired: true
+      })
+    } else {
+      await axios.post('/api/timelog', { ...this.state })
+      this.setState({
+        spent_time: '',
+        estimate_change: '',
+        comment: '',
+      })
+      this.props.getTasksAndLogs();
+    }
   }
   
   render() {
     return (
       <div>
+         <div className='flex justify-center m-4'>
+            {this.state.fieldsRequired ? <div className='text-red-lighter'>All fields required</div> : ""}
+          </div>
         <label className='text-smoke'>Time spent: </label>
         <input
           autoFocus="autofocus"
-          onChange={ (e) => this.setState({ spent_time: e.target.value})}
+          onChange={ (e) => this.setState({ spent_time: e.target.value, fieldsRequired: false })}
           className='input-underlined focus:outline-none my-2 border-grey' 
           type="number"
           value={ this.state.spent_time } />
         <label className='text-smoke'>Estimate change: </label>
         <input
-          onChange={ (e) => this.setState({ estimate_change: e.target.value})} 
+          onChange={ (e) => this.setState({ estimate_change: e.target.value, fieldsRequired: false })} 
           className='input-underlined focus:outline-none my-2 border-grey'
           type="number"
           value={ this.state.estimate_change } /><br/>
         <label className='text-smoke'>Comment: </label>
         <input
-          onChange={ (e) => this.setState({ comment: e.target.value})}
+          onChange={ (e) => this.setState({ comment: e.target.value, fieldsRequired: false })}
           onKeyUp={event => {
             if (event.key === 'Enter') {
               this.newTimelog()
