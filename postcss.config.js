9581565fd34325ff6e1212f797e97a8e837ddd9a
mobydickens <steadmegan@gmail.com>
2019-01-10 12:43:09 -0700

    added purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
+
+
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer'),
-    ],
-};
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    process.env.NODE_ENV==="production" &&  purgecss({
+      content: ['./src/**/*.js', './src/**/*.jsx'],
+      css: ['./src/**/*.css'],
+    })
+  ]
+}
