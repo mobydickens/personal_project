ac86af17f63fb9eb07a13b9491831bc1e54ac7b0
mobydickens <steadmegan@gmail.com>
2019-01-11 15:29:16 -0700

    A little more styling and prettifying code in my edit task/detail modal

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -44,21 +44,29 @@ class EditTask extends Component {
             { !this.state.showTitleInput ? 
               <div className='text-xl'>{this.props.task.title}</div> 
               : 
-              <div className='flex flex-col lg:flex-row'>
+              <div className='flex flex-col lg:flex-row bg-grey-lighter p-1'>
                 <input
                   onChange={ (e) => this.setState({ title: e.target.value}) }
-                  className='input focus:outline-none bg-grey-lighter rounded'
+                  className='input focus:outline-none rounded'
                   value={this.state.title}
                   type="text"/>
-                <button onClick={ () => this.updateTitle() } className='btn-reg bg-blue hover:bg-blue-dark hover:border-blue-dark mx-2'>Save</button>
-                <button onClick={ (e) => e.stopPropagation() || this.setState({showTitleInput: false }) } className='mx-2'>Cancel</button> 
+                <button 
+                  onClick={ () => this.updateTitle() } 
+                  className='p-1 focus:outline-none rounded bg-blue hover:bg-blue-dark hover:border-blue-dark text-sm text-white'>
+                  Save
+                </button>
+                <button 
+                  onClick={ (e) => e.stopPropagation() || this.setState({showTitleInput: false }) } 
+                  className='mx-2 text-smoke text-sm'>
+                  Cancel
+                </button> 
               </div> }
           </div>
           <div className='text-smoke'>{moment(this.props.task.created_at).startOf('hour').fromNow()}</div>
         </div>
         <div 
           onClick={ () => this.setState({ showDescriptionInput: true, description: this.props.task.description }) } 
-          className='m-2 mb-4 text-sm'>
+          className='mb-4 text-sm'>
           { !this.state.showDescriptionInput ?
             <div className='text-sm'>{this.props.task.description}</div>
             :
@@ -68,8 +76,16 @@ class EditTask extends Component {
                 className='input focus:outline-none bg-grey-lighter rounded'
                 value={this.state.description}
                 type="text"/>
-              <button onClick={ () => this.updateDescription() } className='btn-reg bg-blue hover:bg-blue-dark hover:border-blue-dark mx-2'>Save</button>
-              <button onClick={ (e) => e.stopPropagation() || this.setState({ showDescriptionInput: false })} className='mx-2'>Cancel</button> 
+              <button 
+                onClick={ () => this.updateDescription() } 
+                className='p-1 focus:outline-none rounded bg-blue hover:bg-blue-dark hover:border-blue-dark text-sm text-white'>
+                Save
+              </button>
+              <button 
+                onClick={ (e) => e.stopPropagation() || this.setState({ showDescriptionInput: false })} 
+                className='mx-2 text-smoke text-sm'>
+                Cancel
+              </button> 
             </div> 
           }
         </div>
