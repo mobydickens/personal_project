118a421796d6a7964acf4fe2f9321b59a690c28f
mobydickens <steadmegan@gmail.com>
2019-01-12 23:42:21 -0700

    trying to make the main page signup more appealing.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header.jsx';
 import Signup from './Signup.jsx';
+import '../images.css';
 
 class Main extends Component {
 
@@ -19,26 +20,32 @@ class Main extends Component {
 
   render() {
     return (
-      <div>
+      <div className='h-screen'>
         <Header />
         {this.state.showSignup ?
         <Signup signupFn={this.signup}/>
         : 
         <div>
-          <div className='flex m-2 p-2 flex-col lg:m-6 lg:flex-row lg:justify-center'>
+          <div className='flex m-2 p-2 flex-col lg:mx-16 lg:my-8 lg:justify-center'>
             <div className='flex flex-col w-full lg:w-1/2'>
               <div className='font-josefin p-6 text-3xl lg:text-4xl z-10'>Your place for organizing and staying on track.</div>
               <div className='z-10'>
                 <button
                   onClick={ () => this.signup() } 
-                  className='bg-palette-grey hover:bg-blue-dark hover:border-blue-dark text-white border border-palette-grey mx-6 lg:mx-12 py-2 lg:py-4 px-4 lg:px-8 text-xl rounded z-10'>
+                  className='bg-blue hover:bg-blue-dark hover:border-blue-dark text-white border border-blue mx-6 lg:mx-6 py-2 lg:py-4 px-4 lg:px-8 text-xl rounded z-10'>
                   Sign Up
                 </button> 
               </div>
-              <div className='w-full flex justify-center flex-col lg:flex-row'>
-                <div className='m-4 lg:w-1/3 w-full h-48 lg:h-64 border'><div className='bg-newproject'></div></div>
-                <div className='m-4 lg:w-1/3 w-full h-48 lg:h-64 border'><div className=''></div></div>
-                <div className='m-4 lg:w-1/3 w-full h-48 lg:h-64 border'><div className=''></div></div>
+            </div>
+            <div className='w-full flex justify-center flex-col lg:flex-row'>
+              <div className='m-4 p-6 w-full box-height border-2 border-grey rounded'>
+                <div className='bg-newproject'></div>
+              </div>
+              <div className='m-4 w-full border-2 border-grey rounded'>
+                <div className='bg-task'></div>
+              </div>
+              <div className='m-4 p-6 w-full border-2 border-grey rounded'>
+                <div className='bg-feature'></div>
               </div>
             </div>
           </div>
